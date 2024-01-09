c6fa581aa9ba1c45e2c41db751ef6fb78c563ee8
Daniele T <Fupete@users.noreply.github.com>
2024-01-09 13:59:31 +0100

    fix: typo

--- _data/sites/danieletabellini.js
+++ _data/sites/danieletabellini.js
@@ -15,7 +15,7 @@ module.exports = {
 	urls: [
 		"https://danieletabellini.com/",
 		"https://danieletabellini.com/it/",
-		"https://danieletabellini.com/en/"
+		"https://danieletabellini.com/en/",
 		"https://danieletabellini.com/it/idee/",
 		"https://danieletabellini.com/en/ideas/",
 		// Popular Posts
